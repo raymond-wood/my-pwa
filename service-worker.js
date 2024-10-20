a2e5c99b1b77af40a301554e94021eb8efffd95f
raymond-wood <87679808+raymond-wood@users.noreply.github.com>
2024-10-20 08:34:44 -0700

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('app-cache').then(function(cache) {
+      return cache.addAll([
+        './', // Cache the root page
+        './index.html',
+        './scripts.js',
+        './manifest.json',
+        './images/icon-192x192.png',
+        './images/icon-512x512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
